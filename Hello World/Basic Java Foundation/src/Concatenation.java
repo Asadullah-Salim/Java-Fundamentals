a860e07738d685a969655ab3dca0de0a7fac77c6
Asadullah-Salim <83467196+Asadullah-Salim@users.noreply.github.com>
2022-08-03 12:32:02 +0500

    Delete Hello World/Basic Java Foundation directory

--- Hello World/Basic Java Foundation/src/Concatenation.java
+++
@@ -1,9 +0,0 @@
-public class Concatenation {
-    public static void main(String[] args) {
-        double studentGPA = 3.14;
-        String studentFirstName = "Asadullah";
-        String studentLastName = "Salim";
-
-        System.out.println(studentFirstName +" "+ studentLastName +" has a GPA of "+ studentGPA );
-    }
-}
