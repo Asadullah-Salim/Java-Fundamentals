6079c9c2f9f9902bffbb2c9c11d36206d7a6dd67
Asadullah-Salim <maxswim7@gmail.com>
2022-08-03 12:46:07 +0500

    DataTypes,Refrences,InputOutput
    learn about various datatypes, refrence types, and input concatenation

---
+++ Building Strong Foundation/Basic Java/src/Concatenation.java
@@ -0,0 +1,8 @@
+public class Concatenation {
+    public static void main(String[] args) {
+        double studentGPA = 3.45;
+        String studentFirstName = "Asadullah";
+        String studentLastName = "Salim";
+        System.out.println(studentFirstName + " " + studentLastName + " has a GPA of " + studentGPA);
+    }
+}
---
+++ Building Strong Foundation/Basic Java/src/DataTypes.java
@@ -0,0 +1,21 @@
+public class DataTypes {
+    public static void main(String[] args) {
+            int studentAge = 15;
+            String studentFirstName = "Asadullah";
+            String studentLastName = "Salim";
+            double studentGPA = 3.45;
+            char studentFirstInitial = studentFirstName.charAt(0);
+            char studentLastInitial = studentLastName.charAt(0);
+            boolean hasPerfectAttendance = true;
+
+
+            System.out.println(studentAge);
+            System.out.println(studentGPA);
+            System.out.println(studentFirstInitial);
+            System.out.println(studentLastInitial);
+            System.out.println(hasPerfectAttendance);
+            System.out.println(studentFirstName);
+            System.out.println(studentLastName);
+
+    }
+}
